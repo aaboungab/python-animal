030eea7fa96221b11297cfea3abeff2e6dcec866
anas aboungab <aaboungab@qa.com>
2020-11-23 20:35:11 +0000

    .

--- s1/create.py
+++
@@ -1,5 +0,0 @@
-from application import db
-from application.models import Animal
-
-db.drop_all()
-db.create_all()
--- s2/application/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask, request
-import requests
-
-app = Flask(__name__)
-
-from application import routes
