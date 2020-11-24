abe866677bece68ead236aca49c529a2e961fbed
Anas Aboungab <aaboungab@qa.com>
2020-11-24 17:46:33 +0000

    In progress

--- service1/app.py
+++ service1/app.py
@@ -5,10 +5,10 @@ import requests
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET','POST'])
+@app.route('/', methods=['GET'])
 def index():
     animal = requests.get("http://35.242.155.212:5001/animal")
-    noise = requests.post("http://35.242.155.212:5001/animal/noise", data=animal.text)
+    noise = requests.post("http://35.242.155.212:5001/noise", data=animal.text)
 
     return render_template('index.html', animal=animal.text, noise=noise.text)
 
